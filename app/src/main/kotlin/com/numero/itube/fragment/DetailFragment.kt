32ca959570a231fd93089baff15ffcfe24e6e494
NUmeroAndDev <numero.dev.and@gmail.com>
2018-04-25 10:37:00 +0900

    詳細のViewを追加

---
+++ app/src/main/kotlin/com/numero/itube/fragment/DetailFragment.kt
@@ -0,0 +1,41 @@
+package com.numero.itube.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import com.numero.itube.R
+import com.numero.itube.model.Video
+
+class DetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    companion object {
+        private const val ARG_VIDEO = "ARG_VIDEO"
+
+        fun newInstance(video: Video): DetailFragment = DetailFragment().apply {
+            arguments = bundleOf(ARG_VIDEO to video)
+        }
+    }
+}
