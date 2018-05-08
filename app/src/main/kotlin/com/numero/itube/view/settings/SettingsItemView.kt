b85d0e3980e481b2a59e48fc9ec5205cd34a0371
NUmeroAndDev <numero.dev.and@gmail.com>
2018-05-08 15:10:41 +0900

    設定関連のViewのパラメータ追加

--- app/src/main/kotlin/com/numero/itube/view/settings/SettingsItemView.kt
+++ app/src/main/kotlin/com/numero/itube/view/settings/SettingsItemView.kt
@@ -10,11 +10,18 @@ import kotlinx.android.synthetic.main.view_settings_item.view.*
 
 class SettingsItemView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
 
+    var title: String
+        get() = titleTextView.text.toString()
+        set(value) {
+            titleTextView.text = value
+        }
+
     init {
         View.inflate(context, R.layout.view_settings_item, this)
 
         context.withStyledAttributes(attrs, R.styleable.SettingsItemView) {
             titleTextView.text = getString(R.styleable.SettingsItemView_title)
+            dividerView.visibility = if (getBoolean(R.styleable.SettingsItemView_showDivider, true)) View.VISIBLE else View.GONE
         }
     }
 }
