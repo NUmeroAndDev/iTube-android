4b998e2dfdc67e7f91036d4c5dd8f644e6db5e8e
NUmeroAndDev <numero.dev.and@gmail.com>
2018-05-21 11:42:39 +0900

    Preferenceに書き換え

--- app/src/main/kotlin/com/numero/itube/fragment/SettingsFragment.kt
+++ app/src/main/kotlin/com/numero/itube/fragment/SettingsFragment.kt
@@ -2,16 +2,16 @@ package com.numero.itube.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import com.numero.itube.R
-import kotlinx.android.synthetic.main.fragment_settings.*
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
     private var listener: MainSettingsFragmentListener? = null
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
@@ -20,15 +20,12 @@ class SettingsFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        licenseItemView.setOnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val licensesScreen = findPreference("licenses")
+        licensesScreen.setOnPreferenceClickListener {
             listener?.showLicenses()
+            false
         }
     }
 
