b2ba6f1b874bcab25f8b363237dcd003b7bdcc15
NUmeroAndDev <numero.dev.and@gmail.com>
2018-11-02 21:14:08 +0900

    エラーの表示を修正

--- app/src/main/kotlin/com/numero/itube/api/response/RelativeResponse.kt
+++ app/src/main/kotlin/com/numero/itube/api/response/RelativeResponse.kt
@@ -1,6 +1,15 @@
 package com.numero.itube.api.response
 
+import java.lang.Exception
+
 data class RelativeResponse(
         val searchResponse: SearchResponse,
         val channelResponse: ChannelResponse,
-        val videoDetailResponse: VideoDetailResponse)
+        val videoDetailResponse: VideoDetailResponse) {
+
+    fun checkResponse() {
+        if (searchResponse.items.isEmpty() or videoDetailResponse.items.isEmpty()) {
+            throw Exception()
+        }
+    }
+}
