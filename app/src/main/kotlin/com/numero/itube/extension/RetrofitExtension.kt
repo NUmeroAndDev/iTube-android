c15e5822bae2a5335a22a4cd5066cb2df40ea360
NUmeroAndDev <numero.dev.and@gmail.com>
2018-12-10 12:07:44 +0900

    通信周りのエラーハンドリング修正

--- app/src/main/kotlin/com/numero/itube/extension/RetrofitExtension.kt
+++ app/src/main/kotlin/com/numero/itube/extension/RetrofitExtension.kt
@@ -4,11 +4,15 @@ import com.numero.itube.api.response.Result
 import retrofit2.Call
 
 fun <T> Call<T>.executeAsync(): Result<T> {
-    val response = execute()
-    return if (response.isSuccessful) {
-        val body = response.body() ?: return Result.Error(Exception("No response"))
-        return Result.Success(body)
-    } else {
-        Result.Error(Exception("status code : ${response.code()}"))
+    return try {
+        val response = execute()
+        return if (response.isSuccessful) {
+            val body = response.body() ?: return Result.Error(Exception("No response"))
+            return Result.Success(body)
+        } else {
+            Result.Error(Exception("status code : ${response.code()}"))
+        }
+    } catch (t: Throwable) {
+        Result.Error(t)
     }
 }
