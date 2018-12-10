30033b9f1055bfde6614271712676a295c62bf1e
NUmeroAndDev <numero.dev.and@gmail.com>
2018-12-10 12:03:11 +0900

    importを消す

--- app/src/main/kotlin/com/numero/itube/di/ApiClientModule.kt
+++ app/src/main/kotlin/com/numero/itube/di/ApiClientModule.kt
@@ -9,7 +9,6 @@ import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
