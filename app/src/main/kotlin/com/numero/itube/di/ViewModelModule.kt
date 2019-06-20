d66443163f20347d5aa66b8c9c40a2f80a14dfa8
NUmeroAndDev <numero.dev.and@gmail.com>
2019-06-20 17:42:48 +0900

    ViewModelにロジックを持たせてPresenterを削除

--- app/src/main/kotlin/com/numero/itube/di/ViewModelModule.kt
+++ app/src/main/kotlin/com/numero/itube/di/ViewModelModule.kt
@@ -1,13 +1,22 @@
 package com.numero.itube.di
 
-import androidx.lifecycle.ViewModelProvider
-import com.numero.itube.viewmodel.ViewModelFactory
+import androidx.lifecycle.ViewModel
+import com.numero.itube.viewmodel.ChannelVideoListViewModel
+import com.numero.itube.viewmodel.SearchVideoViewModel
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
-abstract class ViewModelFactoryModule {
-    @Binds
-    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
-}
+abstract class ViewModelModule {
 
+    @Binds
+    @IntoMap
+    @ViewModelKey(SearchVideoViewModel::class)
+    abstract fun bindSearchVideoViewModel(viewModel: SearchVideoViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ChannelVideoListViewModel::class)
+    abstract fun bindChannelVideoListViewModel(viewModel: ChannelVideoListViewModel): ViewModel
+}
--- app/src/main/kotlin/com/numero/itube/repository/ChannelRepository.kt
+++ app/src/main/kotlin/com/numero/itube/repository/ChannelRepository.kt
@@ -1,11 +1,15 @@
 package com.numero.itube.repository
 
 import androidx.lifecycle.LiveData
+import com.numero.itube.api.request.ChannelVideoRequest
 import com.numero.itube.api.response.Result
+import com.numero.itube.api.response.VideoResponse
 import com.numero.itube.model.ChannelDetail
+import com.numero.itube.model.ChannelVideoList
 
 interface ChannelRepository {
 
     fun fetchChannelDetail(key: String, channelId: String): LiveData<Result<ChannelDetail>>
 
+    fun fetchChannelVideoList(request: ChannelVideoRequest): LiveData<Result<ChannelVideoList>>
 }
