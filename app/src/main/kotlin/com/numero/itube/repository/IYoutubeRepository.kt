a743668763406d303ae36bd8ac96a81484f9b1ff
NUmeroAndDev <numero.dev.and@gmail.com>
2019-06-20 20:34:37 +0900

    使用しなくなったコードを削除

--- app/src/main/kotlin/com/numero/itube/repository/IYoutubeRepository.kt
+++ app/src/main/kotlin/com/numero/itube/repository/IYoutubeRepository.kt
@@ -1,19 +1,10 @@
 package com.numero.itube.repository
 
-import com.numero.itube.api.request.ChannelVideoRequest
 import com.numero.itube.api.request.RelativeRequest
-import com.numero.itube.api.request.SearchVideoRequest
 import com.numero.itube.api.response.RelativeResponse
 import com.numero.itube.api.response.Result
-import com.numero.itube.api.response.VideoResponse
-import com.numero.itube.model.ChannelDetail
-import com.numero.itube.model.SearchVideoList
 
 interface IYoutubeRepository {
 
     suspend fun loadRelative(request: RelativeRequest): Result<RelativeResponse>
-
-    suspend fun loadChannelVideo(request: ChannelVideoRequest): Result<VideoResponse>
-
-    suspend fun loadChannelDetail(key: String, channelId: String): Result<ChannelDetail>
 }
