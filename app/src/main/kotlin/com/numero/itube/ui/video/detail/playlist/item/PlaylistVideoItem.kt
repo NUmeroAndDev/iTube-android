76fa5e61a26a55a9a24103d79bd97a4fa82c30d8
NUmeroAndDev <numero.dev.and@gmail.com>
2019-09-03 12:19:38 +0900

    Playlistのアイテムのデザイン変更

--- app/src/main/kotlin/com/numero/itube/ui/video/detail/playlist/item/PlaylistVideoItem.kt
+++ app/src/main/kotlin/com/numero/itube/ui/video/detail/playlist/item/PlaylistVideoItem.kt
@@ -8,13 +8,13 @@ import com.numero.itube.R
 import com.numero.itube.model.Video
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
-import kotlinx.android.synthetic.main.view_holder_video.*
+import kotlinx.android.synthetic.main.item_playlist_video.*
 
 class PlaylistVideoItem(
         val video: Video.InPlaylist
 ) : Item() {
 
-    override fun getLayout(): Int = R.layout.view_holder_video
+    override fun getLayout(): Int = R.layout.item_playlist_video
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
         val context = viewHolder.root.context
